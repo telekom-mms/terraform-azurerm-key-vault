4fd1be3d9f73d41542c5e07fdb9b7011e03f673d
mima <Michaela.Mattes@t-systems.com>
2021-12-16 15:05:18 +0100

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "keyvault" {
   description = "azurerm_keyvault results"
   value = {
-    for keyvault in keys(azurerm_key_vault.keyvault):
+    for keyvault in keys(azurerm_key_vault.keyvault) :
     keyvault => {
       name = azurerm_key_vault.keyvault[keyvault].name
       id   = azurerm_key_vault.keyvault[keyvault].id
